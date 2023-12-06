da4719be196372e6845fd096e3f40474c60fb5b5
TejasBhurbhure <tejasbhurbhure06@gmail.com>
2023-12-06 17:39:57 +0530

    added show password button

--- script.js
+++ script.js
@@ -17,3 +17,12 @@ form.addEventListener('submit', function(event) {
   document.getElementById('username').value = '';
   document.getElementById('password').value = '';
 });
+
+function showPassword() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
